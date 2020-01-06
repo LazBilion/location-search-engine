a566a684304f199433ad4dcec799a19c14f9ad43
lazbil <lazbilion@gmail.com>
2020-01-07 00:45:24 +0200

    Adds image as thumbnail and tweaks to the CSS

--- src/Components/SearchComponent/searchComponent.js
+++ src/Components/SearchComponent/searchComponent.js
@@ -2,13 +2,17 @@ import React from "react";
 
 import classes from "./searchComponent.module.css";
 import SearchEngine from "Components/Search-Engine";
-import { FaUser } from "react-icons/fa";
+import thumbnail from "thumbnail/XEthumbnail.png";
 
 const SearchComponent = () => {
   return (
     <div className={classes.searchComponent}>
-      <div className={classes.imageThumbnail}>
-        <FaUser />
+      <div className={classes.thumbnailContainer}>
+        <img
+          className={classes.thumbnailImage}
+          src={thumbnail}
+          alt="XE logo thumbnail"
+        />
       </div>
       <div className={classes.searchInput}>
         <h1>What place are you looking for?</h1>
