8a27b67bb0590940c0c1959f19e60cdacf4d6a22
lazbil <lazbilion@gmail.com>
2020-01-26 14:00:39 +0200

    adds ie9 & ie11 polyfills

--- src/Components/SearchComponent/searchComponent.js
+++ src/Components/SearchComponent/searchComponent.js
@@ -2,17 +2,13 @@ import React from "react";
 
 import classes from "./searchComponent.module.css";
 import SearchEngine from "Components/Search-Engine";
-import thumbnail from "thumbnail/XEthumbnail.png";
+import { FaUser } from "react-icons/fa";
 
 const SearchComponent = () => {
   return (
     <div className={classes.searchComponent}>
       <div className={classes.thumbnailContainer}>
-        <img
-          className={classes.thumbnailImage}
-          src={thumbnail}
-          alt="XE logo thumbnail"
-        />
+        <FaUser size={50} />
       </div>
       <div className={classes.searchInput}>
         <h1>What place are you looking for?</h1>
